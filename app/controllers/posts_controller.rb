f31112f4bbf2aa692ce722f0e2615facfa6b8c8d
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-10-18 18:24:07 -0300

    feat: make #create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to :index
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
